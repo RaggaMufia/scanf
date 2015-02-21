5344f52b16b04f13d7de65cd52a498c61c313e60
Ethan Estrada <ethan.estrada@gmail.com>
2015-02-20 21:40:51 -0700

    Start implementing 'translate' function

--- scanf.py
+++ scanf.py
@@ -7,22 +7,24 @@ cases. This module is designed to work with 2.7 or greater.
 from __future__ import unicode_literals, print_function, absolute_import
 
 import re
+import sys
 
-# account for difference in bytes and strings in py2 and py3
-try:
-    unicode
-except NameError:
-    pass
-else:
+# account for differences in in Python 2 and 3
+if sys.version_info[0] < 3:
+    from future_builtins import ascii, filter, hex, map, oct, zip
     bytes = str
     str = unicode
+    range = xrange
+else:
+    pass
 
 # spec with no groups
-spec = (r'(%%|(?:%(?:\([a-zA-Z_]\w*\))?[#]?[0]?[-]?[ ]?[+]?'
-        r'(?:[0-9]+|[*])?(?:[.][0-9]+|[*])?[diouxXeEfFgGcrs]))')
+_spec = r'(%%|(?:%(?:\(\w+\))?\*?[0-9]*(?:h{1,2}|l{1,2}|j|z|t|L)?[duioxeEfFgGcrs]))'
+_spec = re.compile(_spec)
 
 # spec with groups
-gspec = r'(?P<escaped_percent>%%)|(?:%(?P<kw>\([a-zA-Z_]\w*\))?[#]?[0]?[-]?[ ]?[+]?(?:[0-9]+|[*])?(?:[.][0-9]+|[*])?[diouxXeEfFgGcrs])'
+_gspec = r'(?P<escape>%%)|(?:%(?P<key>\(\w+\))?(?P<skip>\*)?(?P<width>[0-9]+)?(?:h{1,2}|l{1,2}|j|z|t|L)?(?P<spec>[duioxXeEfFgGcrs]))'
+_gspec = re.compile(_gspec)
 
 
 class SF_Pattern(object):
@@ -39,7 +41,7 @@ class SF_Pattern(object):
 
     @property
     def type(self):
-        """Return the return type from scan method.
+        """The return type from scan method.
 
         This will be either a tuple or a dictionary type object.
         """
@@ -68,9 +70,69 @@ def _compile_pattern(pat):
 
 def translate(scanf_spec):
     """Translate a scanf format specifier into a regular expression string."""
+    split = _spec.split(scanf_spec)
+    print(split)
+
+    strlst = []
+    for i, s in enumerate(split):
+        if i % 2 == 0:
+            # split on whitespace
+            split = s.split()
+            print(split)
+
+            # escape characters that might cause problems with regex
+            for j in range(len(split)):
+                split[j] = re.escape(split[j])
+            print(split)
+
+            # replace whitespace to consume all whitespace
+            join = r'\s+'.join(split)
+            print(join)
+            strlst.append(join)
+        else:
+            sfd = _gspec.match(s).groupdict()
+            print(sfd)
+
+            if sfd['escape']:
+                strlst.append(r'\%')
+                continue
+
+            if sfd['skip']:
+                grpstrt = '(?:'
+            elif sfd['key']:
+                grpstrt = '(?<' + sfd['key'] + '>'
+            else:
+                grpstrt = '('
+
+            if sfd['spec'] == 'i':
+                spec = r'[-+]?(?:0[xX][\dA-Fa-f]+|0[0-7]*|\d+)'
+            elif sfd['spec'] == 'd':
+                spec = r'[-+]?\d+'
+            elif sfd['spec'] == 'o':
+                spec = r'[-+]?[0-7]+'
+            elif sfd['spec'].lower() == 'x':
+                spec = r'[-+]?(0[xX])?[\dA-Fa-f]+'
+
+            if sfd['width']:
+                pass
+            strlst.append(s)
+
+    print(strlst)
     return scanf_spec
 
 
-def scan(format, string):
-    """Return either a tuple or a dictionary of parsed values."""
-    pass
+def scan(fmt, string):
+    """Scan the provided string.
+
+    Return either a tuple or a dictionary of parsed values, or None if the
+    string did not conform to the format."""
+    re_fmt = compile(fmt)
+    return re_fmt.scan(string)
+
+
+def _test():
+    translate('.punct*$uation @ %d middle %s almost end %c')
+    translate('%d middle %s almost end %c')
+
+if __name__ == '__main__':
+    _test()
